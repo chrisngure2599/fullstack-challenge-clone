6765ed2f9382d147add14d39701e649fe6625cc8
Chris Ngure <chrisngure2599@gmail.com>
2023-03-03 00:42:15 +0300

    Ref: Ran lint cmd

--- frontend/src/components/ApiTest.vue
+++ frontend/src/components/ApiTest.vue
@@ -3,79 +3,73 @@ import WeatherDetailsModal from "@/components/WeatherDetailsModal.vue";
 export default {
   data: () => ({
     apiResponse: null,
-    statusCode:null,
-    currentUserDetails:null
+    statusCode: null,
+    currentUserDetails: null,
   }),
   components: {
-    WeatherDetailsModal
+    WeatherDetailsModal,
   },
 
   created() {
-    this.fetchData()
+    this.fetchData();
   },
-  computed:{
-    
-  },
+  computed: {},
   methods: {
     weather_details(user_weather_data){
-      this.currentUserDetails=user_weather_data;
+    weatherDetails(user_weather_data) {
+      this.currentUserDetails = user_weather_data;
     },
-    async fetchData() {
-      const url = 'http://localhost:8000'
-      const response = await fetch(url)
-      
-      this.statusCode = response.status // Get the status code
-      if(this.statusCode==200){
-        this.apiResponse = await response.json()
-      }
-      
 
-    }
-  }
-}
+    async fetchData() {
+      const url = "http://localhost:8000";
+      const response = await fetch(url);
+
+      this.statusCode = response.status; // Get the status code
+      if (this.statusCode == 200) {
+        this.apiResponse = await response.json();
+      }
+    },
+  },
+};
 </script>
 
 <template>
-  <div v-if="!statusCode">
-    Loading the data.....
-  </div>
-  <div v-if="statusCode==204">
-      <h1>No Users found</h1>
+  <div v-if="!statusCode">Loading the data.....</div>
+  <div v-if="statusCode == 204">
+    <h1>No Users found</h1>
   </div>
 
-  <div v-if="statusCode>=500 " >
+  <div v-if="statusCode >= 500">
     <h1>Internal error please try again later</h1>
   </div>
-
-  
 
   <div v-if="apiResponse">
     <h1>Users ({{ apiResponse.users.length }}).</h1>
     <code>
       <!-- {{ apiResponse }} -->
-    <!-- Looping through the Users -->
-    <div v-if="apiResponse.users" >
-      <table class="table table-striped table-hover">
-        <thead>
-          <tr>
-            <th>
-              S/N
-            </th>
-            <th>
-              User Name
-            </th>
-            <th>
-              EMail
-            </th>
-            <th>
-              Weather Summary.
-            </th>
-            <th>Lat</th>
-            <th>Long</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr @click="weather_details(user)" v-for="user in apiResponse.users" title="Click for more" data-bs-toggle="modal" data-bs-target="#myModal" >
+      <!-- Looping through the Users -->
+      <div v-if="apiResponse.users">
+        <table class="table table-striped table-hover">
+          <thead>
+            <tr>
+              <th>S/N</th>
+              <th>User Name</th>
+              <th>EMail</th>
+              <th>Weather Summary.</th>
+              <th>Lat</th>
+              <th>Long</th>
+              <th>Updated at</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              @click="weatherDetails(user)"
+              v-for="(user, index) in apiResponse.users"
+              title="Click for more"
+              data-bs-toggle="modal"
+              v-bind:key="index"
+              data-bs-target="#myModal"
+            >
               <td>
                 {{ user.id }}
               </td>
@@ -86,11 +80,14 @@ export default {
                 {{ user.email }}
               </td>
               <td>
-                <div v-if="user.weather_data.status" >
-                  <img :src="user.weather_data.condition_icon" alt=""> 
+                <div v-if="user.weather_data.status">
+                  <img :src="user.weather_data.condition_icon" alt="" />
                 </div>
-                <div v-else :title="user.weather_data.error_message"  class="text-danger" >
-                
+                <div
+                  v-else
+                  :title="user.weather_data.error_message"
+                  class="text-danger"
+                >
                   Error!
                 </div>
               </td>
@@ -100,20 +97,26 @@ export default {
               <td>
                 {{ user.latitude }}
               </td>
-          </tr>
-        </tbody>
-      </table>
-      <!-- The Modal -->
-<div class="modal fade" id="myModal" >
-  <div class="modal-dialog">
-    <div  class="modal-content">
-      <WeatherDetailsModal  :weather_data=currentUserDetails ></WeatherDetailsModal >
-    </div>
-  </div></div>
-    </div>
-    <div v-else >
-      <h1>No Users found</h1>
-    </div>
+              <td>
+                {{ Date(user.updated_at).toLocaleString() }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <!-- The Modal -->
+        <div class="modal fade" id="myModal">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <WeatherDetailsModal
+                :weather_data="currentUserDetails"
+              ></WeatherDetailsModal>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div v-else>
+        <h1>No Users found</h1>
+      </div>
     </code>
   </div>
 </template>
