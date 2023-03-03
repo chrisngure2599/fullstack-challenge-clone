b28f0d5e10d4005cac576f73e3875e54152736c7
Chris Ngure <chrisngure2599@gmail.com>
2023-03-03 08:51:36 +0300

    Added test_job_get_users_weather_works

--- api/tests/Feature/ExampleTest.php
+++ api/tests/Feature/ExampleTest.php
@@ -3,8 +3,10 @@
 namespace Tests\Feature;
 
 use App\Models\User;
+use App\Models\UsersWeather;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Jobs\GetUsersWeather;
 
 class ExampleTest extends TestCase
 {
@@ -18,8 +20,16 @@ class ExampleTest extends TestCase
     public function test_the_application_returns_a_successful_response()
     {
         $response = $this->get('/');
+        $response->assertStatus(204);// Returning empty if no Users.
+
+        User::factory(20)->create();
+        $response = $this->get('/');
 
         $response->assertStatus(200);
+
+       
+        
+
     }
 
     public function test_database_works()
@@ -27,5 +37,14 @@ class ExampleTest extends TestCase
         User::factory(20)->create();
 
         $this->assertEquals(20, User::all()->count());
+    }
+    
+    //Test the Job if works.
+    public function test_job_get_users_weather_works()
+    {
+        $this->assertEquals(0, UsersWeather::get()->count()); //Making sure no data before
+        User::factory(2)->create();
+        dispatch(new GetUsersWeather);
+        $this->assertEquals(2, UsersWeather::get()->count());        
     }
 }
